f111fda85b3b1095edeceb44cc3eb60a79622675
Romaissa <100511292+Romaissa-code@users.noreply.github.com>
2022-09-02 14:53:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    const toggleBtn=document.getElementsByClassName('toggle')[0];
+    const navlink=document.getElementsByClassName('nav-bar-link')[0];
+                toggleBtn.addEventListener('click',()=>{
+                navlink.classList.toggle('active')}
+                )}
